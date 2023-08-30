d2f6d56e9891f86b2341455a3acdbf429d0d08ff
Guy <guy@Guys-MacBook-Pro.local>
2023-08-30 09:52:58 -0500

    remove dockerfile and docs

--- docs/assets/Card-5ec8c550.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-959d0f8a.js";function e(r){return a.jsx("div",{className:`${r.className}  p-4 rounded shadow bg-slate-50 dark:bg-primary-900/50 `,children:r.children})}export{e as C};
