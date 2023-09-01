3fca900b9bef93aef4ffec12015711ea43bb6419
Guy <guy@Guys-MBP.midcoip.net>
2023-09-01 15:22:36 -0500

    StartPlaying Link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,18 +9,19 @@ const styles = {
     padding: '16px',
     margin: 'auto 16px',
   }
-}
+} as any;
 
 export default function Footer() {
   const { toggleDark } = useDarkMode()
   function changeLang(lang: string) {
     i18n.changeLanguage(lang)
   }
+  const linkClass = 'p-2 text-center flex'
   return (
     <footer style = {styles.footer}>
       <div className="flex justify-center w-full pb-4">
         <nav className="flex mt-6 text-xl" dir="utd">
-          <div>
+          <div className={linkClass}>
             <a rel="noopener noreferrer" href={'https://www.patreon.com/critfumbleweb'} target="_blank">
               <img
                 src={"/img/patreon.avif"}
@@ -28,8 +29,8 @@ export default function Footer() {
                 alt={'Support us on Patreon'}
               />
             </a>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-
+          </div>
+          <div className={linkClass}>
             <a rel="noopener noreferrer" href={'https://startplaying.games/gm/crit-fumble/review'} target="_blank">
               <img
                 src={"/img/startplaying.png"}
@@ -37,25 +38,35 @@ export default function Footer() {
                 alt={'Leave us a Review on StartPlaying.Games'}
               />
             </a>
-            {/* <a rel="noopener noreferrer" href={https://affiliates.fantasygrounds.com/324256/15958/banner_7299} target="_blank">
+          </div>
+          <div className={linkClass}>
+            <a rel="noopener noreferrer" href={'https://app.roll20.net/users/6244861/crit-fumble-gaming'} target="_blank">
+              <img
+                src={"/img/roll20.png"}
+                height={50}
+                alt={'View our Roll20 Profile'}
+              />
+            </a>
+          </div>
+          <div className={linkClass}>
+            <a rel="noopener noreferrer" href={'https://affiliates.fantasygrounds.com/324256/15958/banner_7299'} target="_blank">
               <img
                 src={"https://affiliates.fantasygrounds.com/img/banners/316034_3264676826.jpg"}
                 height={50}
                 alt={'Shop on Fantasy Grounds'}
-              />
-            </a> */}
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <a rel="noopener noreferrer" href={'https://discord.gg/dZzsst6TdG'} target="_blank">
-              <img
-                src={"/img/discord.avif"}
-                height={50}
-                alt={'Join our Discord Server'}
               />
             </a>
           </div>
         </nav>
       </div>
       <div className={'text-center'}>
+        <a rel="noopener noreferrer" href={'https://discord.gg/dZzsst6TdG'} target="_blank">
+          <img
+            src={"/img/discord.avif"}
+            height={50}
+            alt={'Join our Discord Server'}
+          />
+        </a><br />
         <button onClick={toggleDark} style={{height: '1.6em', width: '1.6em', marginBottom: '16px'}} className="p1 cursor-pointer bg-transparent transition-colors duration-300 border-none  decoration-none text-primary-500 hover:bg-primary-600 hover:text-white font-light rounded-full centered !outline-none text-xl" >
           <div i="carbon-sun dark:carbon-moon " />
         </button>
