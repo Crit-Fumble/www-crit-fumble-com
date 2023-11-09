b1c2ab5588ce9760180abf5322206a1336331eb7
Guy <the0bob@users.noreply.github.com>
2023-11-09 10:37:13 -0600

    Updates

---
+++ assets/Card-4e5c4bef.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-28ca0aa9.js";function e(r){return a.jsx("div",{className:`${r.className}  p-4 rounded shadow bg-slate-50 dark:bg-primary-900/50 `,children:r.children})}export{e as C};
